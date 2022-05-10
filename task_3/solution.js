94643368ebcf4f6f3a8266f5d22cc6b84d0caa03
Evelina Zueva <evelina20096@yandex.ru>
2022-05-10 13:15:02 +0300

    изменение

--- task_3/solution.js
+++ task_3/solution.js
@@ -24,10 +24,10 @@ function renderCartItem(item) {
     // 8. Присвойте переменной itemCountText результат склеивания itemCountText и ` ₽`
 let itemCountText = " ";
 itemCountText = itemCountText + product.count;
-itemCountText = itemCountText + " * ";
-itemCountText = itemCountText + product.prise;
+itemCountText = itemCountText + " × ";
+itemCountText = itemCountText + product.price;
 itemCountText = itemCountText + " ₽ = ";
-let sum = product.count * product.prise;
+let sum = product.count * product.price;
 itemCountText = itemCountText + sum;
 itemCountText = itemCountText + " ₽";
     // Конец решения задания №3.1.
