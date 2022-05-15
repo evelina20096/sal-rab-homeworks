6be08652e2175b2f2a865c5b4e31028084b9eeb9
Evelina Zueva <evelina20096@yandex.ru>
2022-05-15 19:11:21 +0300

    исправление

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,7 +34,7 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, order: {address, sum}, goods: [goods.title, goods.count]};
 
     let countOfGoods = goods.length;
 
