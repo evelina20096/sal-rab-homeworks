14827a006cbd0114e03ca85b1886e18a828b6552
Evelina Zueva <evelina20096@yandex.ru>
2022-05-17 22:31:35 +0300

    b

--- task_final/solution.js
+++ task_final/solution.js
@@ -37,15 +37,15 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = {client: {name, phone}, order: {address, sum}, goods: []};
 
     let countOfGoods = goods.length;
-как
+
     for (let i = 0; i < countOfGoods; i += 1) {
         data.goods.push({title: goods[i].title, count: goods[i].count});
     }
 
-    data.order.address = street, house, entrance, floor, flat;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
     data.order.sum = sum;
 
-    data.client = 'Иван ' + phone;
+    data.client = name + " " + phone;
 
     let jsonData = JSON.stringify({data});
 
